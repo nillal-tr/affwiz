0f1e674eaeadd8fb8bc925ea66f0a78595fd4f05
nillyal <nillyal@etoro.com>
2023-01-09 16:51:52 +0200

    formValidation

--- src/app/aff-types/add-edit-aff-type/add-edit-aff-type.component.ts
+++ src/app/aff-types/add-edit-aff-type/add-edit-aff-type.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 
 interface ITierCalcMethod {
@@ -16,46 +16,60 @@ interface ITierCalcMethod {
   styleUrls: ['./add-edit-aff-type.component.css']
 })
 export class AddEditAffTypeComponent implements OnInit {
+  firstCTvalue: number = 9;
+  secondCTvalue: number = 19;
+  thirdCTvalue: number = 29;
+  firstSCvalue: number = 9;
+  secondSCvalue: number = 19;
+  thirdSCvalue: number = 29;
+
 
   affTypeForm = new FormGroup({
     description: new FormControl(''),
     notes: new FormControl(''),
     tierMethod: new FormControl(''),
     tiers: new FormControl(''),
+      tier1Rate: new FormControl(''),
+      tier2Rate: new FormControl(''),
+      tier3Rate: new FormControl(''),
+      tier4Rate: new FormControl(''),
+      tier5Rate: new FormControl(''),
+
     cookieExpiration: new FormControl('60'),
     hideTrackingLinks: new FormControl(''),
     hideCreatives: new FormControl(''),
     categories: new FormControl(''),
 
     copyTrader: new FormControl(''),
-      copyTrader1: new FormControl(''),
+      copyTrader1: new FormControl('9', Validators.max(999)),
       copyTrader1B: new FormControl(''),
-      copyTrader2: new FormControl(''),
+      copyTrader2: new FormControl('19', Validators.max(999)),
       copyTrader2B: new FormControl(''),
-      copyTrader3: new FormControl(''),
+      copyTrader3: new FormControl('29', Validators.max(999)),
       copyTrader3B: new FormControl(''),
       copyTrader4: new FormControl(''),
 
     depositCommission: new FormControl(''),
-    depositCommissionCPA1: new FormControl(''),
+    depositCommissionCPA1: new FormControl('9', Validators.max(999)),
     depositCommissionCPA1B: new FormControl(''),
-    depositCommissionCPA2: new FormControl(''),
+    depositCommissionCPA2: new FormControl('19', Validators.max(999)),
     depositCommissionCPA2B: new FormControl(''),
-    depositCommissionCPA3: new FormControl(''),
+    depositCommissionCPA3: new FormControl('29', Validators.max(999)),
     depositCommissionCPA3B: new FormControl(''),
     depositCommissionCPA4: new FormControl(''),
     depositCommissionCPAD1: new FormControl(''),
-    depositCommissionCPAD2: new FormControl(''),
-
+    depositCommissionCPAD2: new FormControl('1'),
+    depositCommissionCPAD3: new FormControl(''),
+    depositCommissionCPAD4: new FormControl(''),
 
     commissionDropDown: new FormControl(''),
     openPositionReq: new FormControl(''),
     saleCommission: new FormControl(''),
-      saleCommission1: new FormControl(''),
+      saleCommission1: new FormControl('9', Validators.max(999)),
       saleCommission1B: new FormControl(''),
-      saleCommission2: new FormControl(''),
+      saleCommission2: new FormControl('19', Validators.max(999)),
       saleCommission2B: new FormControl(''),
-      saleCommission3: new FormControl(''),
+      saleCommission3: new FormControl('29', Validators.max(999)),
       saleCommission3B: new FormControl(''),
       saleCommission4: new FormControl(''),
 
@@ -73,8 +87,7 @@ export class AddEditAffTypeComponent implements OnInit {
     viewTieredAffDetail: new FormControl(''),
     createCustomLinksAffConsole: new FormControl('checked'),
 
-    tier2Rate: new FormControl(''),
-
+    
 
   })
 
@@ -91,6 +104,8 @@ export class AddEditAffTypeComponent implements OnInit {
     {value: '5', viewValue: '5'}
   ]
 
+  tiersSubFields: any[] = [];
+
   commissions: any[] = [
     {value: 'CPA', viewValue: 'CPA'},
     {value: 'CPAD', viewValue: 'CPAD'}
@@ -101,4 +116,14 @@ export class AddEditAffTypeComponent implements OnInit {
   constructor () {}
   ngOnInit () {}
 
+  onSelectChange(event: any) {
+    console.log(this.tiersSubFields)
+    this.tiersSubFields = [];
+    if(event !== 1) {
+      for(let i=1; i<event.value; i++) {
+        this.tiersSubFields.push(i);
+      }
+      console.log(this.tiersSubFields)
+    }
+  }
 }
