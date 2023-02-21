3f0c5c3087fa6645a9b8a2c9c6e2ccb677ac91e9
nillyal <nillyal@etoro.com>
2023-02-21 12:57:54 +0200

    210221+TieredAffDisplay_refactoring

--- src/app/aff-types/add-edit-aff-type/tiered-aff-display-options/tiered-aff-display-options.component.ts
+++ src/app/aff-types/add-edit-aff-type/tiered-aff-display-options/tiered-aff-display-options.component.ts
@@ -1,5 +1,7 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { FormControlSettingsTieredAffDisplay } from 'src/app/models/form-control-settings-tiered-aff-display.model';
+import { FormControlService } from 'src/app/shared/bl/form-control/form-control.service';
 
 @Component({
   selector: 'app-tiered-aff-display-options',
@@ -10,22 +12,44 @@ import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
   ],
 })
 export class TieredAffDisplayOptionsComponent {
-  affTypeFormTieredAffDisplayOption: FormGroup;
+  // affTypeFormTieredAffDisplayOption: FormGroup;
+  affTypeFormTieredAffDisplayGroup: FormGroup = this.fb.group({});
 
   @Output() affTypeFormTieredAffDisplayOptionEvent = new EventEmitter<any>();
 
-  constructor(private formBuilder: FormBuilder) {
-    this.affTypeFormTieredAffDisplayOption = this.formBuilder.group({
-      viewTieredAffCount: new FormControl(''),
-      viewTieredAffDetail: new FormControl(''),
+  constructor(
+    // private formBuilder: FormBuilder,
+    private fb: FormBuilder,
+    private formControlService: FormControlService
+    ) {
+    // this.affTypeFormTieredAffDisplayOption = this.formBuilder.group({
+    //   viewTieredAffCount: new FormControl(''),
+    //   viewTieredAffDetail: new FormControl(''),
+    // });
+  }
+
+  ngOnInit() {
+    this.createForm();
+
+    setTimeout(() => {
+      this.affTypeFormTieredAffDisplayGroup;
+    }, 10000);
+  }
+
+  createForm() {
+    this.formControlService.setFormControls({
+      fb: this.fb,
+      fg: this.affTypeFormTieredAffDisplayGroup,
+      controlsSettings: FormControlSettingsTieredAffDisplay,
     });
   }
 
+
   // output of the form to the parent component
   addNewItem() {
-    if (this.affTypeFormTieredAffDisplayOption.valid) {
+    if (this.affTypeFormTieredAffDisplayGroup.valid) {
       this.affTypeFormTieredAffDisplayOptionEvent.emit(
-        this.affTypeFormTieredAffDisplayOption.controls
+        this.affTypeFormTieredAffDisplayGroup.controls
       );
     }
   }
---
+++ src/app/models/form-control-settings-tiered-aff-display.model.ts
@@ -0,0 +1,12 @@
+import { Validators } from '@angular/forms';
+
+export const FormControlSettingsTieredAffDisplay: any[] = [
+  {
+    name: 'viewTieredAffCount',
+    validators: []
+  },
+  {
+    name: 'viewTieredAffDetail',
+    validators: []
+  }
+]
